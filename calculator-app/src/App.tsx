7f655c7bbfa501db64479cb6cbe6efdc6ac6593d
Дмитрий Попков <dmitriypopkov98@gmail.com>
2022-04-18 23:36:48 +0300

    react init

---
+++ calculator-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export function App() {
+  return (
+    <div className="wrapper">
+      <div
+        className="content"
+        draggable={true}
+      ></div>
+    </div>
+  );
+}
